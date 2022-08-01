ba4a6436da2fb16a5b86b86db7de19c955cc6374
Ant0nymous <ant0nymous@protonmail.com>
2022-08-01 10:35:59 +0800

    Updated results message

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function Calculate() {
   let bridgeT = document.getElementById("bridgeTap").value;
   let newDeltD = deltD - 50
   let internal = newDeltD > bridgeT
-  if (internal === true) {
+  if (internal === false) {
     tech = "Copy into your notes<br><br>Estimated DELT Distance " + deltD + "m - 50m = " + newDeltD + "m<br>Bridge Tap Distance = " + bridgeT + "m<br>DELT Distance is > Bridge Tap Distance <br>Internal Bridge Tap!<br>Paid Telstra Technician, Electrician or <br>ACMA Cabler Required to Remove Bridge Tap"
   }
-  else if (internal === false) {
+  else if (internal === true) {
     tech = "Copy into your notes<br><br>Estimated DELT Distance " + deltD + "m - 50m = " + newDeltD + "m<br>Bridge Tap Distance = " + bridgeT + "m<br>DELT Distance is < Bridge Tap Distance <br>External Bridge Tap!<br>NBN Technician Required to Remove Bridge Tap"
   }
   document.getElementById("tech").innerHTML = tech.toString();
