b1afd67ccd9ea0510cf71da8c96eb18444eca80a
Ant0nymous <ant0nymous@protonmail.com>
2022-08-01 10:34:30 +0800

    Updated results message

--- Website Versions/Version 6/index.js
+++ Website Versions/Version 6/index.js
@@ -5,11 +5,11 @@ function Calculate() {
   let bridgeT = document.getElementById("bridgeTap").value;
   let newDeltD = deltD - 50
   let internal = newDeltD > bridgeT
-  if (internal === false) {
-    tech = "Copy into your notes<br><br>Estimated DELT Distance " + deltD + "m - 50m = " + newDeltD + "m<br>Bridge Tap Distance = " + bridgeT + "m<br>DELT Distance > Bridge Tap Distance <br>Internal Bridge Tap!<br>Paid Telstra Technician, Electrician or <br>ACMA Cabler Required to Remove Bridge Tap"
+  if (internal === true) {
+    tech = "Copy into your notes<br><br>Estimated DELT Distance " + deltD + "m - 50m = " + newDeltD + "m<br>Bridge Tap Distance = " + bridgeT + "m<br>DELT Distance is > Bridge Tap Distance <br>Internal Bridge Tap!<br>Paid Telstra Technician, Electrician or <br>ACMA Cabler Required to Remove Bridge Tap"
   }
-  else if (internal === true) {
-    tech = "Copy into your notes<br><br>Estimated DELT Distance " + deltD + "m - 50m = " + newDeltD + "m<br>Bridge Tap Distance = " + bridgeT + "m<br>DELT Distance < Bridge Tap Distance <br>External Bridge Tap!<br>NBN Technician Required to Remove Bridge Tap"
+  else if (internal === false) {
+    tech = "Copy into your notes<br><br>Estimated DELT Distance " + deltD + "m - 50m = " + newDeltD + "m<br>Bridge Tap Distance = " + bridgeT + "m<br>DELT Distance is < Bridge Tap Distance <br>External Bridge Tap!<br>NBN Technician Required to Remove Bridge Tap"
   }
   document.getElementById("tech").innerHTML = tech.toString();
   }
