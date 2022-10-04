462ceba38e62861bc95bccdab166cfc1ef13ef59
Ant0nymous <ant0nymous@protonmail.com>
2022-10-04 11:27:40 +0800

    3rd update of JS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function Calculate() {
   let deltD = document.getElementById("deltDistance").value;
   let bridgeT = document.getElementById("bridgeTap").value;
   let newDeltD = deltD - 50
-  let internal = bridgeT > newDeltD
+  let internal = bridgeT < newDeltD
   if (internal === false) {
     tech = "Copy into your notes<br><br>Estimated DELT Distance " + deltD + "m - 50m = " + newDeltD + "m<br>Bridge Tap Distance = " + bridgeT + "m<br>Bridge Tap Distance is greater than Est DELT Distance<br>Internal Bridge Tap!<br>Paid Telstra Technician, Electrician or <br>ACMA Cabler Required to Remove Bridge Tap"
   }
